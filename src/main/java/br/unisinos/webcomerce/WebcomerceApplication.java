ee038094676ae670a042fdcc9fa562f9705d76c9
JULIAN RAFAEL DE SOUZA <julian93bully@gmail.com>
2026-05-10 14:02:40 -0300

    feat(webcomerce): start repository

---
+++ src/main/java/br/unisinos/webcomerce/WebcomerceApplication.java
@@ -0,0 +1,13 @@
+package br.unisinos.webcomerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebcomerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebcomerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/unisinos/webcomerce/WebcomerceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.unisinos.webcomerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebcomerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
